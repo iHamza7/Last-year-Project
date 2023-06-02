8a6a8924531c928b2352223e4cb6ee9dbc3d9250
iHamza7 <khanazam3324@gmail.com>
2023-06-02 20:23:22 +0500

    lib changed

--- lib/cnic_findupload/cnic_show.dart
+++ lib/cnic_findupload/cnic_show.dart
@@ -99,7 +99,7 @@ class _ShowCnicState extends State<ShowCnic> {
                         debugPrint(args.ownerNumber.text);
                         debugPrint(args.ownerExpire.text);
                       },
-                      child: const Text('next please'),
+                      child: const Text('next here'),
                     ),
                   ],
                 ),
