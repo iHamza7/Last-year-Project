73d6217f39450acfdd8a5c24363f9f8fe1d0ea83
iHamza7 <khanazam3324@gmail.com>
2023-06-03 14:14:35 +0500

    lib changed

---
+++ lib/cnic_findupload/text_field.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class TextInputField extends StatelessWidget {
+  final String? labelText;
+  final TextEditingController controller;
+  const TextInputField(this.labelText, this.controller, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: labelText,
+          labelStyle:
+              const TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+          fillColor: Colors.white,
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.black,
+              width: 2.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
